884fe4e92bc66f2a296149e4630b8ccb7d8e00ef
Jhonni test <Jhonni888@yandex.ru>
2023-06-22 21:55:43 +0300

    Verify code done

--- src/test/java/ru/netology/Github_Action/statistic/ServiceTest.java
+++ src/test/java/ru/netology/Github_Action/statistic/ServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.Github_Action.statistic;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ServiceTest {
 
     @Test
@@ -8,8 +10,44 @@ public class ServiceTest {
         ru.netology.Github_Action.statistic.Service service = new Service();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
 
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxOne() {
+        ru.netology.Github_Action.statistic.Service service = new Service();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 12, 8, 6, 11, 11, 12};
+
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxTwo() {
+        ru.netology.Github_Action.statistic.Service service = new Service();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 12, 12};
+
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxThree() {
+        ru.netology.Github_Action.statistic.Service service = new Service();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 10, 12};
+
+        long expected = 12;
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
